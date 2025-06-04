9df44d1eac58ccbec266442bf849f35b40d4b379
Matheus Maia <mathmferreira@hotmail.com>
2025-06-04 12:56:55 -0300

    chore: upgraded spring boot version to 3.5.0 and spring dependency management to 1.1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
 	java
 	`maven-publish`
 	`java-test-fixtures`
-	id("org.springframework.boot") version "3.2.2"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("org.springframework.boot") version "3.5.0"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.techmath"
